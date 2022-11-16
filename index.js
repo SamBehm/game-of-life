292244cfb57aca1dfb22ea76a554e788bfdc7ca3
Sam Behm <sambehmwork@gmail.com>
2022-11-16 15:06:23 +1000

    Added drawing capability, fixed some bugs causing the cells never to be updated properly

--- index.js
+++ index.js
@@ -4,24 +4,121 @@ const Status = {
 }
 
 class Cell {
-        constructor(status) {
-                this.status = status;
+        constructor(s) {
+                this.status = s;
                 this.newStatus = Status.DEAD;
         }
 }
 
-var currentKey;
 var canvas;
 var context;
 
-const numCellsX = 200;
+const cellGap = 2;      /* space between each cell */
 var cellDimensions;
+
+const numCellsX = 100;
 var numCellsY;
 
-var cells;
 
-const tickRate = 7; // updates / second
+var cells;              /* 2d array of cells */
+
+var currentKey;
+var mouseDown = false;
+
+const tickRate = 4;     /* updates / second */
 var gameRunning = false;
+
+function getCellPosition(screenX, screenY) {
+        let x = Math.floor((screenX - cellGap) / (cellDimensions + cellGap));
+        let y = Math.floor((screenY - cellGap) / (cellDimensions + cellGap));
+        return [x, y];
+}
+
+function checkNeighbours(x, y) {
+        let numAlive = 0;
+        for (let i = -1; i < 2; i++) {
+                for (let j = -1; j < 2; j++) {
+                        if (i == 0 && j == 0) {
+                                continue;
+                        }
+
+                        if (x + i < 0 || x + i >= numCellsX) {
+                                continue;
+                        }
+
+                        if (y + j < 0 || y + j >= numCellsY) {
+                                continue;
+                        }
+
+                        if (cells[x + i][y + j].status == Status.ALIVE) {
+                                numAlive++;
+                        }
+
+                        if (numAlive > 3) {
+                                return 4;
+                        }
+                }
+        }
+        return numAlive;
+}
+
+function updateCell(x, y) {
+        let n = checkNeighbours(x, y); // number of alive neighbours
+
+        if (cells[x][y].status == Status.ALIVE) {
+                cells[x][y].newStatus = (n != 2 && n != 3) ? Status.DEAD : Status.ALIVE;
+        } else {
+                cells[x][y].newStatus = (n == 3) ? Status.ALIVE : Status.DEAD;
+        }
+}
+
+function updateCells() {
+        for (let x = 0; x < numCellsX; x++) {
+                for (let y = 0; y < numCellsY; y++) {
+                        updateCell(x, y);
+                }
+        }
+
+        for (let x = 0; x < numCellsX; x++) {
+                for (let y = 0; y < numCellsY; y++) {
+                        cells[x][y].status = cells[x][y].newStatus;
+
+                        if (cells[x][y].status == Status.ALIVE) draw(x, y);
+                }
+        }
+}
+
+function clear() {
+        context.fillStyle = "black";
+        context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function updateCanvas() {
+        clear();
+        updateCells();
+        for (let x = 0; x < numCellsX; x++) {
+                for (let y = 0; y < numCellsY; y++) {
+                        if (cells[x][y].status == Status.ALIVE) {
+                                draw(x, y);
+                        }
+                }
+        }
+}
+
+function gameLoop() {
+        if (gameRunning) {
+                updateCanvas();
+                setTimeout(gameLoop, 1000 / tickRate);
+        }
+}
+
+function draw(cellX, cellY) {
+        console.log(`${cellX}, ${cellY}`);
+        context.fillStyle = "white";
+        context.fillRect((cellX * cellDimensions) + (cellX * cellGap),
+                (cellY * cellDimensions) + (cellY * cellGap),
+                cellDimensions, cellDimensions);
+}
 
 
 function initListeners() {
@@ -42,6 +139,41 @@ function initListeners() {
                 }
 
         });
+
+        document.addEventListener("mousedown", (event) => {
+                if (event.isComposing || gameRunning) return;
+
+                if (!mouseDown) {
+                        let mouseX = event.clientX - canvas.offsetLeft;
+                        let mouseY = event.clientY - canvas.offsetTop;
+                        let cellPosition = getCellPosition(mouseX, mouseY);
+                        draw(cellPosition[0], cellPosition[1]);
+                }
+
+                mouseDown = true;
+        });
+
+        document.addEventListener("mouseup", (event) => {
+                if (event.isComposing) return;
+                mouseDown = false;
+        });
+
+        document.addEventListener("mouseout", (event) => {
+                if (event.isComposing) return;
+                mouseDown = false;
+        });
+
+        document.addEventListener("mousemove", (event) => {
+                if (event.isComposing) return;
+
+                if (mouseDown) {
+                        let mouseX = event.clientX - canvas.offsetLeft;
+                        let mouseY = event.clientY - canvas.offsetTop;
+                        let cellPosition = getCellPosition(mouseX, mouseY);
+                        cells[cellPosition[0]][cellPosition[1]].status = Status.ALIVE;
+                        draw(cellPosition[0], cellPosition[1]);
+                }
+        });
 }
 
 function init() {
@@ -51,9 +183,10 @@ function init() {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
 
-        cellDimensions = canvas.width / (numCellsX + 1);
-        numCellsY = Math.floor(canvas.height / cellDimensions);
+        let remaindingWidth = canvas.width - ((numCellsX + 1) * cellGap);
+        cellDimensions = remaindingWidth / numCellsX;
 
+        numCellsY = Math.floor((canvas.height - cellGap) / (cellDimensions + cellGap));
         cells = new Array(numCellsX);
 
         gameRunning = false;
@@ -68,84 +201,8 @@ function init() {
                 }
         }
 
-        cells[20][20].status == Status.ALIVE;
-        cells[21][20].status == Status.ALIVE;
-        cells[20][21].status == Status.ALIVE;
-        cells[21][21].status == Status.ALIVE;
+        clear();
+
 
         initListeners();
-}
-
-
-function checkNeighbours(x, y) {
-        let numAlive = 0;
-        for (let i = -1; i < 2; i++) {
-                for (j = -1; j < 2; j++) {
-                        if (i == 0 && j == 0) continue;
-
-                        if (x + i < 0 || x + i >= numCellsX) {
-                                continue;
-                        }
-
-                        if (y + j < 0 || y + j >= numCellsY) {
-                                continue;
-                        }
-
-                        if (cells[x + i][y + j].status == Status.ALIVE) {
-                                numAlive++;
-                        }
-                }
-        }
-        return numAlive;
-}
-
-function updateCell(x, y) {
-        let n = checkNeighbours(x, y); // number of alive neighbours
-
-        if (cells[x][y].status == Status.ALIVE) {
-                cells[x][y].newStatus = (n != 2 && n != 3) ? Status.DEAD : Status.ALIVE;
-                console.log(`${x} ${y}`);
-        } else {
-                cells[x][y].newStatus = (n == 3) ? Status.ALIVE : Status.DEAD;
-        }
-}
-
-function updateCells() {
-        for (let x = 0; x < numCellsX; x++) {
-                for (let y = 0; y < numCellsY; y++) {
-                        updateCell(x, y);
-                }
-        }
-
-        for (let x = 0; x < numCellsX; x++) {
-                for (let y = 0; y < numCellsY; y++) {
-                        cells[x][y].status = cells[x][y].newStatus;
-                }
-        }
-}
-
-function clear() {
-        context.fillStyle = "black";
-        context.fillRect(0, 0, canvas.width, canvas.height);
-}
-
-function updateCanvas() {
-        clear();
-        updateCells();
-
-        for (let x = 0; x < numCellsX; x++) {
-                for (let y = 0; y < numCellsY; y++) {
-                        if (cells[x][y].status == Status.ALIVE) {
-                                context.filLStyle = "white";
-                                context.fillRect(x + 1, y + 1, cellDimensions, cellDimensions);
-                        }
-                }
-        }
-}
-
-function gameLoop() {
-        if (gameRunning) {
-                updateCanvas();
-                setTimeout(gameLoop, 1000 / tickRate);
-        }
 }
